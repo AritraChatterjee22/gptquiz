763b412a31edd309fae420caf06682d4f8d1137b
Aritra <chatterjee.aritra@protonmail.com>
2023-08-25 12:44:39 +0530

    mcq game

--- src/app/play/mcq/[gameId]/page.tsx
+++ src/app/play/mcq/[gameId]/page.tsx
@@ -1,3 +1,6 @@
+import { prisma } from '@/lib/db';
+import { getAuthSession } from '@/lib/nextauth';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 type Props = {
@@ -7,8 +10,29 @@ type Props = {
   }
 
 const MCQPage = async ({params: {gameId}}: Props) => {
+  const session = await getAuthSession()
+  if(!session?.user) {
+      return redirect('/')
+    }
+    const game = await prisma.game.findUnique({
+        where: {
+            id: gameId
+          },
+        include: {
+            questions: {
+                select: {
+                    id: true,
+                    question: true,
+                    options: true,
+                  }
+              }
+          }   
+      })
+    if(!game || game.gameType !== 'mcq') {
+        return redirect('/quiz')
+      }
   return (
-    <div>{gameId}</div>
+    <pre>{JSON.stringify(game, null, 2)}</pre>
   )
 }
 
