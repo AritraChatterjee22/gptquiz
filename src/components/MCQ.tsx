02b7559772a4e4411ba33dd2f251245f08aba9c4
Aritra <chatterjee.aritra@protonmail.com>
2023-08-30 18:26:04 +0530

    submit page and answer counter

--- src/components/MCQ.tsx
+++ src/components/MCQ.tsx
@@ -1,15 +1,17 @@
 'use client'
 import { Game, Question } from '@prisma/client'
 import React from 'react'
-import { LuChevronRight, LuTimer } from 'react-icons/lu'
+import { LuChevronRight, LuTimer, LuLoader2, LuBarChart } from 'react-icons/lu'
 import { Card, CardDescription, CardHeader, CardTitle } from './ui/card'
-import { Button } from './ui/button'
+import { Button, buttonVariants } from './ui/button'
 import MCQCounter from './MCQCounter'
 import {useMutation} from "@tanstack/react-query"
 import axios from "axios"
 import { z } from 'zod'
 import { checkAnswerSchema } from '@/schemas/form/quiz'
 import { useToast } from './ui/use-toast'
+import Link from "next/link";
+import { cn } from '@/lib/utils'
 
 type Props = {
     game: Game & {questions: Pick<Question, 'id' | 'options' | 'question'>[]}
@@ -27,6 +29,9 @@ const MCQ = ({game}: Props) => {
 
   //react state to count the wrong answers
   const [wrongAnswers, setWrongAnswers] = React.useState<number>(0)
+
+  //react state for end of quiz
+  const [hasEnded, setHasEnded] = React.useState<boolean>(false)
 
   //shadcn toast
   const {toast} = useToast()
@@ -70,10 +75,14 @@ const MCQ = ({game}: Props) => {
                       })
                     setWrongAnswers((prev) => prev + 1)
                   }
+                  if(questionIndex === game.questions.length - 1){
+                      setHasEnded(true);
+                      return;
+                    }
                   setQuestionIndex((prev) => prev + 1)
             }
         })
-    },[checkAnswer, toast, isChecking])
+    },[checkAnswer, toast, isChecking, questionIndex, game.questions.length])
 
   
   //Adding keyboard controls
@@ -107,6 +116,20 @@ const MCQ = ({game}: Props) => {
       return JSON.parse(currentQuestion.options as string) as string[];
     },[currentQuestion])
 
+  if(hasEnded) {
+      return(
+        <div className='absolute flex flex-col justify-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+          <div className='px-4 mt-2 font-semibold text-white bg-green-500 rounded-md whitespace-nowrap'>
+            You completed in {'6 mins 9 secs'}
+          </div>
+          <Link href={`/statistics/${game.id}`} className={cn(buttonVariants(),'mt-2')}>
+            View Statistics
+            <LuBarChart className='w-4 h-4 ml-2'/>
+          </Link>
+        </div>
+      )
+    }
+
   return (
     <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-[80vw] max-w-4xl w-[90vw]'>
       <div className='flex flex-row justify-between'>
@@ -121,7 +144,7 @@ const MCQ = ({game}: Props) => {
           <span>00:00</span>
         </div>
         </div>
-        <MCQCounter correctAnswers={3} wrongAnswers={3}/>
+        <MCQCounter correctAnswers={correctAnswers} wrongAnswers={wrongAnswers}/>
       </div>
       <Card className='w-full mt-4'>
         <CardHeader className='flex flex-row items-center'>
@@ -162,6 +185,7 @@ const MCQ = ({game}: Props) => {
               handleNext();
             }}
           >
+          {isChecking && <LuLoader2 className='w-4 h-4 mr-2 animate-spin'/>}
             Next <LuChevronRight className='w-4 h-4 ml-2'/>
           </Button>
       </div>
