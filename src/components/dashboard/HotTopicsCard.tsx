cc7fb34208c23a0b1f177d6806ae9d02c8356aef
Aritra <chatterjee.aritra@protonmail.com>
2023-09-02 00:58:20 +0530

    wordcloud and quiz based on wordcloud

--- src/components/dashboard/HotTopicsCard.tsx
+++ src/components/dashboard/HotTopicsCard.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card'
 import CustomWordCloud from '../CustomWordCloud'
+import { prisma } from '@/lib/db'
 
 type Props = {}
 
-const HotTopicsCard = (props: Props) => {
+const HotTopicsCard = async (props: Props) => {
+  const topics = await prisma.topic_count.findMany({})
+  const formattedTopics = topics.map(topic =>{
+      return{
+          text: topic.topic,
+          value: topic.count
+        }
+    })
   return (
     <Card className='col-span-4'>
       <CardHeader>
@@ -13,7 +21,7 @@ const HotTopicsCard = (props: Props) => {
           Click on a topic to start a quiz on it!
         </CardDescription>
       </CardHeader>
-      <CardContent className='pl-2'><CustomWordCloud/></CardContent>
+      <CardContent className='pl-2'><CustomWordCloud formattedTopics={formattedTopics}/></CardContent>
     </Card>
   )
 }
